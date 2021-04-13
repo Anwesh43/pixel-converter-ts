e53497a9ca6e389dc73bb8c86db8c5726150238e
Anwesh43 <anweshthecool0@gmail.com>
2021-04-14 03:13:43 +0530

    adding DrawingUtil and PixelContainer class

--- index.ts
+++ index.ts
@@ -3,6 +3,56 @@ const h : number = window.innerHeight
 const scGap : number = 0.02 
 const delay : number = 20 
 const sizeFactor : number = 2 
+const imageFactor : string = `car`;
+const size : number = Math.min(w, h) / sizeFactor
+
+class PixelContainer {
+
+    pixels  : Array<Uint8ClampedArray> = []
+
+    loadImage(i : number) {
+        const r : HTMLImageElement = new Image()
+        r.src = `${imageFactor}${i}.jpg`
+        r.onload = () => {
+            const canvas : HTMLCanvasElement = document.createElement('canvas')
+            canvas.width = size 
+            canvas.height = size 
+            const context = canvas.getContext('2d')
+            context.drawImage(r, 0, 0, size, size)
+            const imageData : ImageData  = context.getImageData(0, 0, size, size)
+            this.pixels.push(imageData.data)
+            console.log("img", imageData.data)
+        }
+    }
+
+    getPixels(i : number) : Uint8ClampedArray {
+        return this.pixels[i]
+    }
+}
+
+const pixelContainer : PixelContainer = new PixelContainer()
+
+for (var j = 1; j < 6; j++) {
+    pixelContainer.loadImage(j)
+}
+
+class DrawingUtil {
+
+    static drawPixelImage(context : CanvasRenderingContext2D, images : Array<HTMLImageElement>, i : number, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const pixel1 : Uint8ClampedArray = pixelContainer.getPixels(i)
+        const pixel2 : Uint8ClampedArray = pixelContainer.getPixels(i)
+        const imageData : ImageData = context.getImageData(0, 0, size, size)
+        const data = imageData.data;
+        for (var j = 0; j < pixel1.length; j++) {
+            data[j] = pixel1[j] + (pixel2[j] - pixel1[j]) * scale 
+            data[j + 1] = pixel1[j + 1] + (pixel2[j + 1] - pixel1[j + 1]) * scale
+            data[j + 2] = pixel1[j + 2] + (pixel2[j + 2] - pixel1[j + 2]) * scale
+        }
+         
+        context.putImageData(imageData, 0, 0)
+    }
+}
 
 class State {
 
